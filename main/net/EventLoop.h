7235502dea99ac9f5e64a28efd902709594b0252
Lixin-SCUT <im.lixin@foxmail.com>
2020-02-13 00:05:42 +0800

    2020.02.13

--- main/net/EventLoop.h
+++ main/net/EventLoop.h
@@ -1,7 +1,86 @@
-//EventLoop.h
-//Created by Lixin on 2020.02.11
-//Version 0.1
+// EventLoop.h
+// Created by Lixin on 2020.02.11
+// Version 0.2
 
+#ifndef MAIN_NET_EVENTLOOP_H
+#define MAIN_NET_EVENTLOOP_H
+
+class Channel;
+class Poller;
+class TimerQueue;
+
+// Reactor,one thread one loop
+// interface class
+
+#include <atomic>
+#include <functional>
+#include <vector>
+
+#include <boost/any.hpp>
+
+#include "main/base/Mutex.h"
+#include "main/base.CurrentThread.h"
+#include "main/base/Timestamp.h"
+#include "main/net/Callbacks.h"
+#include "main/net/TimerId.h"
+
+namespace main{
+
+namespace net{
+	
+class EventLoop : noncopyable
+{
+public:
+	typedef std::fuction<void()> Functor;
+
+	EventLoop();
+	~EventLoop();
+
+	//Loops forever
+	void loop();
+	//Quits loop
+	void quit();
+
+	Timerstamp pollReturnTime() const {return pollReturnTime_;}
+
+	int64_t iteration() const {return tireation_;}
+
+private:
+	void abortNotInLoopThread();
+	void handleRead(); // waked up;
+	void doPendingFunctors();
+
+	void printActiveChannels() const; // DEBUG 
+
+	typedef std::vector<Channel*> ChannelList;
+
+	bool looping_; // atomic
+	std::atomic<bool> quit_;
+	bool eventHandling_; // atomic;
+	bool callingPendingFunctors_; // atomic
+	int64_t iterarion_;
+	const pid_t threadId_;
+	Timestamp pollReturnTime_;
+	std::unique_ptr<Poller> poller_;
+	std::unique_ptr<TimerQueue> timerQueue_;
+	int wakeunFd_;
+	std::unique_ptr<Channel> wakeupChannel_;
+	boost::any context_;
+
+	ChannelList activeChannels_;
+	Channel* currentActiveChannel_;
+
+	mutable MutexLock mutex_;
+	std::vector<Functor> pendingFunctors_ GUARDED_BY(mutex_);
+};
+
+}// namespace net 
+}// namespace main
+
+#endif //MAIN_NET_EVENT_LOOP_H
+
+/*
+//Version 0.1
 #include <boost/utility.hpp>
 #include <pthread.h>
 
@@ -9,10 +88,12 @@
 #include "Poller.h"
 #include "TimerQueue.h"
 
+
 class EventLoop{
 public:
 	EventLoop();
 	~EventLoop();
+	
 	void loop();
 	static EventLoop* getEventLoopOfCurrentThread();
 private:
@@ -20,3 +101,4 @@ private:
 	bool isInLoopThread();
 	bool assertInLoopThread();
 }
+*/
