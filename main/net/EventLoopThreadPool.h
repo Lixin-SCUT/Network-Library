a7d7debf1614a322c1a47cefb3c4e3f89b045209
Lixin-SCUT <im.lixin@foxmail.com>
2020-03-07 18:17:13 +0800

    2020.03.07

--- main/net/EventLoopThreadPool.h
+++ main/net/EventLoopThreadPool.h
@@ -3,10 +3,10 @@
 
 #pragma once
 
-#include "/main/net/EventLoopThread.h"
-#include "/main/base/Logging.h"
-#include "/mian/base/noncopyable.h"
+#include "/main/base/noncopyable.h"
+#include "/main/base/Types.h"
 
+#include <functional>
 #include <memory>
 #include <vector>
 
@@ -15,18 +15,46 @@ namespace main
 namespace net
 {
 
+class EventLoop;
+class EventLoopThread();
+
 class EventLoopThreadPool : noncopyable
 {
 public:
-	EventLoopThreadPool（EventLoop* baseLoop, int numThreads）;
+	typedef std::function<void (EventLoop*)> ThreadInitCallback;
+	
+	EventLoopThreadPool(EventLoop* baseLoop, const string& nameArg);
+  	~EventLoopThreadPool();
+  	void setThreadNum(int numThreads) { numThreads_ = numThreads; }
+	// valid after calling start()
+  	// round-robin
+  	EventLoop* getNextLoop();
 
-	～EventLoopThreadPool（）{ LOG_INFO << "~EventLoopThreadPool()"; }
+  	// with the same hash code, it will always return the same EventLoop
+  	EventLoop* getLoopForHash(size_t hashCode);
+
+	std::vector<EventLoop*> getAllLoops();
+	
+	void start(const ThreadInitCallback& cb = ThreadInitCallback());
+	
+	bool started() const
+	{	return started_; }
+	
+	const string& name() const
+	{	return name_; }	
+	
+	/*
+	EventLoopThreadPool（EventLoop* baseLoop, int numThreads;
+
+	~EventLoopThreadPool();
 	void start();
 
 	EventLoop* getNextLoop();
-
+	*/
 private:
-	EventLoop *loop;
+	EventLoop *baseLoop_;
+	//EventLoop *loop;
+	string name_;
 	bool started_;
 	int numThreads_;
 	int next_;
