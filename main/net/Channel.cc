780cf0fcf34dec10e569f70cb67d29c0266d90a5
Lixin-SCUT <im.lixin@foxmail.com>
2020-03-13 20:44:00 +0800

    2020.03.14

--- main/net/Channel.cc
+++ main/net/Channel.cc
@@ -1,139 +1,59 @@
 // Channel.cc
 // Created by Lixin on 2020.02.14
 
-#include "main/base/Logging.h"
-#include "main/net/Channel.h"
-#include "main/net/EventLoop.h"
+#include "Channel.h"
 
-#include <sstream>
+#include "Epoll.h"
+#include "EventLoop.h"
+#include "Util.h"
 
-#include <poll.h>
+#include <unistd.h>
+#include <cstdlib>
+#include <iostream>
+#include <queue>
 
-using namespace main;
-using namespace main::net;
+Channel::Channel(EventLoop *loop)
+	: loop_(loop), 
+	  events_(0), 
+	  lastEvents_(0), 
+	  fd_(0) 
+{ }
 
-const int Channel::kNoneEvent = 0;
-const int Channel::kReadEvent = POLLIN | POLLRPI;
-const int Channel::kWriteEvent = POLLOUT;
+Channel::Channel(EventLoop *loop, int fd)
+	: loop_(loop), 
+	  fd_(fd), 
+	  events_(0), 
+	  lastEvents_(0) 
+{ }
 
-Channel::Channel(EventLoop* loop, int fd_)
-	: loop_(loop),
-	  fd_(fd_),
-	  events_(0),
-	  revents_(0),
-	  index_(-1),
-	  logHup(true),
-	  tied_(false),
-	  eventHandling_(false),
-	  addedToLoop_(false)
-{
+Channel::~Channel() {
+	// loop_->poller_->epoll_del(fd, events_);
+	// close(fd_);
 }
 
-Channel::~Channel()
+int Channel::getFd() { return fd_; }
+void Channel::setFd(int fd) { fd_ = fd; }
+
+void Channel::handleRead() 
 {
-	assert(!eventHandling_);
-	assert(!addedToLoop_);
-	if(loop_->isInLoopThread())
+  	if (readHandler_) 
 	{
-		assert(!loop_->hasChannel(this));
+    		readHandler_();
 	}
 }
 
-void Channel::tie(const std::shared_ptr<void>& obj)
+void Channel::handleWrite() 
 {
-	tie_=obj;
-	tied_ = true;
+	if (writeHandler_) 
+	{
+		writeHandler_();
+	}
 }
 
-void Channel::update()
+void Channel::handleConn() 
 {
-	addedToLoop_ = true;
-	loop_->updateChannel(this);
-}
-
-void Channel::remove()
-{	
-	assert(isNoneEvent());
-	addedToLoop_ = false;
-	loop_>removeChannel(this);
-}
-
-void Channel::handleEvent(Timestamp receiveTime)
-{
-	std::shared_ptr<void> guard;
-	if(tied_)
+	if (connHandler_) 
 	{
-		guard = tie_.lock();
-		if(guard)
-		{
-			handleEventWithGuard(receiveTime);
-		}
-	}
-	else
-	{
-		handleEventWithGuard(receiveTime);
-	}
-
-}
-
-void Channel::handleEventWithGuard(Timestamp receiveTime)
-{
-	eventHandlint_ = true;
-	LOG_TRACE << reventsToString();
-	if((revents_ & POLLHUP) && !(revents_ & POLLIN))
-	{
-		if(logHup_)
-		{
-			LOG_WARM << "fd = " << fd_ << " Channel::handle_event() POLLHUP";
-		}
-		if(closeCallback_) closeCallback_();
-	}
-
-	if(revents_ & POLLNVAL)
-	{
-		LOG_WARM << "fd = " << fd_ << " Channel::handle_event() PLLNVAL";
-	}
-
-	if(revents_ & (POLLERR | POLLNVAL))
-	{
-		if(errorCallback_) errorCallback_();
-	}
-
-	if(revents_ & (POLLIN | POLLPRI | POLLRDHUP))
-	{
-		if(readCallback_) readCallback_(receiveTime);
-	}
-
-	if(revents_ & POLLOUT)
-	{
-		if(writeCallback_) writeCallback_();
-	}
-	eventHandling_=false;
-}
-
-string Channel::reventsToString() const
-{
-	return eventsToString(fd_,events);
-}
-
-string Channel::eventsToString(int fd,int ev)
-{
-	std::ostringstream oss;
-	oss << fd << ": ";
-	if(ev & POLLIN)
-		oss << "IN ";
-	if(ev & POLLPRT)
-		oss << "PRI ";
-	if(ev & POLLOUT)
-		oss << "OUT ";
-	if(ev & POLLHUP)
-		oss << "HUP ";
-	if(ev & POLLRDHUP)
-		oss << "RDHUP ";
-	if(ev & POLLERR)
-		oss << "ERR "
-	if(ev & POLLNVAL)
-		oss << "NVAL ";	
-
-	return oss.str();
+    		connHandler_();
+  	}
 }
