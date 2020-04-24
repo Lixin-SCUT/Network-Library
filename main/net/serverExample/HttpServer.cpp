6b007b514e1446d99db5abceed8f36e2c34b31bb
Lixin-SCUT <im.lixin@foxmail.com>
2020-04-24 23:10:28 +0800

    Version 2.0

--- main/net/serverExample/HttpServer.cpp
+++ main/net/serverExample/HttpServer.cpp
@@ -2,3 +2,102 @@
 // Created by 黎鑫 on 2020/4/23.
 //
 
+#include "HttpServer.h"
+#include "../log/Logging.h"
+#include "../TimerManager.h"
+
+using std::placeholders::_1;
+using std::placeholders::_2;
+
+HttpServer::HttpServer(EventLoop *loop, const int port, const int threadnum)
+    : server_(loop, port, threadnum),
+      mutex_(),
+      connect_list_(),
+      timer_list_()
+{
+    server_.SetNewConnectionCallBack(bind(&HttpServer::HandleNewConnection, this, _1));
+    server_.SetMessageCallback(bind(&HttpServer::HandleMessageCallBack, this, _1, _2));
+    server_.SetSendCompleteCallBack(bind(&HttpServer::HandleSendComplete, this, _1));
+    server_.SetCloseCallBack(bind(&HttpServer::HandleClose, this, _1));
+    server_.SetErrorCallBack(bind(&HttpServer::HandleError, this, _1));
+
+    TimerManager::GetTimerManagerInstance()->StartManager();
+}
+
+HttpServer::~HttpServer()
+{}
+
+void HttpServer::Start()
+{
+    server_.Start();
+}
+
+void HttpServer::HandleNewConnection(const shared_ptr<TcpConnection> connection)
+{
+    LOG << "INFO : New connetion connect successfully!";
+    shared_ptr<HttpSession> http_session(new HttpSession());
+    shared_ptr<Timer> timer(new Timer(Timer::TimerType::ONCE, 4096, bind(&TcpConnection::ShutDown, connection)));
+    timer->Start();
+    {
+        MutexLockGuard lock(mutex_);
+        connect_list_.insert(make_pair(connection, http_session));
+        timer_list_.insert(make_pair(connection, timer));
+    }
+}
+
+void HttpServer::HandleMessageCallBack(const shared_ptr<TcpConnection> connection, string &str)
+{
+    shared_ptr<HttpSession> http_session;
+    shared_ptr<Timer> timer;
+
+    {
+        MutexLockGuard lock(mutex_);
+        http_session = connect_list_[connection];
+        timer = timer_list_[connection];
+    }
+
+    timer->Adjust(Timer::TimerType::ONCE, 4096,bind(&TcpConnection::ShutDown, connection));
+
+    HttpRequestContext http_request_context;
+    string response_context;
+
+    bool result = http_session->PraseHttpRequest(str, http_request_context);
+
+    if(!result)
+    {
+        http_session->HttpError(400, "Bad Request", http_request_context, response_context);
+    }
+    else
+    {
+        http_session->HttpProcess(http_request_context, response_context);
+    }
+    connection->Send(response_context);
+    return;
+}
+
+void HttpServer::HandleSendComplete(const shared_ptr<TcpConnection> connection)
+{
+
+}
+
+void HttpServer::HandleClose(const shared_ptr<TcpConnection> connection)
+{
+    {
+        MutexLockGuard lock(mutex_);
+        connect_list_.erase(connection);
+        timer_list_.erase(connection);
+    }
+}
+
+void HttpServer::HandleError(const shared_ptr<TcpConnection> connection)
+{
+    {
+        MutexLockGuard lock(mutex_);
+        connect_list_.erase(connection);
+        timer_list_.erase(connection);
+    }
+    LOG << "ERROR : error occurs in TcpConnection " << connection->GetFd()
+        << " and close the connection." ;
+}
+
+
--- main/net/serverExample/HttpServer.h
+++ main/net/serverExample/HttpServer.h
@@ -5,9 +5,44 @@
 #ifndef MYPROJECT_HTTPSERVER_H
 #define MYPROJECT_HTTPSERVER_H
 
+#include "../base/noncopyable.h"
 #include "../EventLoop.h"
 #include "../TcpConnection.h"
 #include "../TcpServer.h"
-#include "../HttpSession.h"
+#include "../base/MutexLock.h"
+#include "../Timer.h"
+#include "HttpSession.h"
+
+#include <functional>
+#include <string>
+#include <map>
+
+using std::function;
+using std::bind;
+using std::string;
+
+class HttpServer : noncopyable
+{
+public:
+    HttpServer(EventLoop* loop, const int port, const int threadnum = 0);
+    ~HttpServer();
+
+    void Start();
+
+private:
+    void HandleNewConnection(const shared_ptr<TcpConnection> connection);
+    void HandleMessageCallBack(const shared_ptr<TcpConnection> connection, string& str);
+    void HandleSendComplete(const shared_ptr<TcpConnection> connection);
+    void HandleClose(const shared_ptr<TcpConnection> connection);
+    void HandleError(const shared_ptr<TcpConnection> connection);
+
+private:
+    TcpServer server_;
+
+    MutexLock mutex_;
+    map<shared_ptr<TcpConnection>, shared_ptr<HttpSession>> connect_list_;
+    map<shared_ptr<TcpConnection>, shared_ptr<Timer>> timer_list_;
+
+};
 
 #endif //MYPROJECT_HTTPSERVER_H
